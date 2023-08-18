a8f10dcd46cc431d66d3b1dab13b7f86bf524a9e
iyutpo <y15653862659@outlook.com>
2023-08-18 14:23:05 -0700

    added home page template

--- eat-together/src/App.js
+++ eat-together/src/App.js
@@ -3,13 +3,6 @@ import React, { useState, Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './components/HomePage';
-// import Login from './components/Login';
-import Signup from './components/Signup';
-import NormalLoginForm from './components/Login';
-
-
-// ref: https://rn.mobile.ant.design/docs/react/introduce
-// ref: https://landing.ant.design/docs/use/create-react-app-cn
 
 
 
--- eat-together/src/components/HomePage.js
+++ eat-together/src/components/HomePage.js
@@ -1,35 +1,119 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '../index.css';
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Layout, Menu, Button, theme } from 'antd';
+const { Header, Sider, Content } = Layout;
 
 
 
-function HomePage() {
-
-
-    // define handlers
-    const handleLogin = (event) => {
-        console.log('Login successfully~');
-    }
-
-
-    const handleSignup = (event) => {
-        console.log('Sign up successfully~');
-    }
-
-
-    // return page:
-    return (
-        <div>
-            <h1>Welcome to our website</h1>
-            <button onClick={handleLogin}>Login</button>
-            <button onClick={handleSignup}>Sign Up</button>
-        </div>
-    )
-}
-
-
-
-
-
-
-
+const HomePage = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <Layout>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical" />
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={['1']}
+          items={[
+            {
+              key: '1',
+              icon: <UserOutlined />,
+              label: 'nav 1',
+            },
+            {
+              key: '2',
+              icon: <VideoCameraOutlined />,
+              label: 'nav 2',
+            },
+            {
+              key: '3',
+              icon: <UploadOutlined />,
+              label: 'nav 3',
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header
+          style={{
+            padding: 0,
+            background: colorBgContainer,
+          }}
+        >
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: '16px',
+              width: 64,
+              height: 64,
+            }}
+          />
+        </Header>
+        <Content
+          style={{
+            margin: '24px 16px',
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
 export default HomePage;
+
+
+// function HomePage() {
+
+
+    // // define handlers
+    // const handleLogin = (event) => {
+    //     console.log('Login successfully~');
+    // }
+
+
+    // const handleSignup = (event) => {
+    //     console.log('Sign up successfully~');
+    // }
+
+
+    // // return page:
+    // return (
+    //     <div>
+    //         <h1>Welcome to our website</h1>
+    //         <button onClick={handleLogin}>Login</button>
+    //         <button onClick={handleSignup}>Sign Up</button>
+    //     </div>
+    // )
+// }
+
+// export default HomePage;
+
+
+
+
+
+
+
+
+
+
+
+
+
