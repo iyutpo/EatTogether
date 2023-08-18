7d2c9a763baa919f7d279c27df0338484efa4b7d
y1141335509 <y15653862659@outlook.com>
2023-08-17 23:17:35 -0700

    added ugly login page with ant-design

--- eat-together/src/App.js
+++ eat-together/src/App.js
@@ -3,9 +3,9 @@ import React, { useState, Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './components/HomePage';
-import Login from './components/Login';
+// import Login from './components/Login';
 import Signup from './components/Signup';
-
+import NormalLoginForm from './components/Login';
 
 
 // ref: https://rn.mobile.ant.design/docs/react/introduce
@@ -17,18 +17,19 @@ import Signup from './components/Signup';
 
 
 
-// function App() {
-//   return (
-//     <Router>
-//       <nav>
-//         <Link to="/">Home</Link>
-//       </nav>
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
+function App() {
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
+    
+  );
+}
   
 
-// export default App;
+export default App;
